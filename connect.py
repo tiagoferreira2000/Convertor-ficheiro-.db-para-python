8610bd908ef44da5477872aa55fb60058a268b39
tiagoferreira2000 <tiagoacf21@gmail.com>
2025-05-13 10:41:26 +0200

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,31 @@
+import sqlite3
+import csv
+
+
+con = sqlite3.connect("bd/gestor.db")
+cursor = con.cursor()
+
+
+cursor.execute("""
+    SELECT pedidos.idpedidos, pedidos.idusuario, usuarios.nome AS nome_usuario, pedidos.idproduto, produtos.descricao AS descricao_produto, pedidos.data, pedidos.quantidade         
+    FROM pedidos
+    JOIN usuarios ON pedidos.idusuario = usuarios.idusuario
+    JOIN produtos ON pedidos.idproduto = produtos.idproduto
+""")
+
+
+dados = cursor.fetchall()
+
+
+colunas = [descricao[0] for descricao in cursor.description]
+
+
+with open('gestor.csv', 'w', encoding='utf-8') as ficheiro_csv:
+    escritor_csv = csv.writer(ficheiro_csv)
+    escritor_csv.writerow(colunas)
+    escritor_csv.writerows(dados)
+
+
+con.close()
+
+print("Ficheiro CSV criado com sucesso!")
